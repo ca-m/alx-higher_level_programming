ac932fd89adfe3af6349074c4d072637aa9c1ea5
ca-m <deconnick.carmel@gmail.com>
2023-04-12 10:54:34 -0700

    readme and task 0

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+"""defines text file-reading function""
+
+
+def read_file(filename=""):
+	"""Print the contents of a UTF8 text file to stdout.""
+	with open(filename, encoding=ütf-8") as f:
+		print(f.read(), end="")
---
+++ 0x0B-python-input_output/README.md
@@ -0,0 +1,2 @@
+python project
+0x0B - input-output
